ee80e77a57d4f09c63794d530afd28e1b53cf8b3
fenix <fenix.azul09@protonmail.com>
2022-12-19 17:40:01 -0300

    começo

---
+++ script.js
@@ -0,0 +1,35 @@
+/*<html>
+    <input></input>
+    <button></button>
+
+    <textarea class="text" rows="5" id="textArea"></textarea>
+</html>*/
+
+// botão -> evento click.
+document.getElementById("botaoCriptografar").addEventListener("click", function () {
+
+// texto = valor do input.
+var texto = document.querySelector("input").value;
+
+// Função de Mudar as letras
+function mudarLetra(texto) {
+    
+    // se texto for com acento
+    if(texto == texto.normalize('NFD').replace(/[\u0300-\u036f]/g, "")) {
+
+        texto = texto.replaceAll('e', 'enter')
+        .replaceAll('i', 'imes')
+        .replaceAll('a', 'ai')
+        .replaceAll('o', 'ober')
+        .replaceAll('u', 'ufat');
+        
+        return texto.toLowerCase();
+    } else {
+
+        alert("Só pode letras sem acentos!!!");
+    }
+}
+
+// o valor de textArea = texto.
+document.getElementById("resultado").value = mudarLetra(texto);
+})

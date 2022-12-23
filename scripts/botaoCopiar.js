223e0948c5c9d3b4b74b69cb433125afbaf49ef3
fenix <fenix.azul09@protonmail.com>
2022-12-23 16:23:33 -0300

    função copiar para celular

--- scripts/botaoCopiar.js
+++ scripts/botaoCopiar.js
@@ -1,12 +1,15 @@
 // Função de Copiar o Texto da caixa de resultado
 document.getElementById("copy").addEventListener("click", function () {
 
-    var copyText = document.getElementById("text-resultado");
+    let copyText = document.getElementById("text-resultado");
     
     copyText.select();
     copyText.setSelectionRange(0, 99999);
 
+    // o botão recebe o texto copiado -> navegador pc
     navigator.clipboard.writeText(copyText.value);
-
     alert("o texto foi copiado: " + copyText.value);
+
+    // o botão recebe o texto copiado -> celular
+    document.getElementById("copy") = navigator.clipboard.writeText(copyText.value);
 })
